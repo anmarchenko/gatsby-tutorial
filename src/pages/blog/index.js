4cc9d6123a13ccbeb693368ffc8ecae8e28b6cc7
Andrey Marchenko <andrey.marchenko@share-now.com>
2021-12-27 14:38:20 +0100

    dynamic file routing

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,18 +1,18 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My blog posts">
       {data.allMdx.nodes.map(node => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>
+              {node.frontmatter.title}
+            </Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>
-            {node.body}
-          </MDXRenderer>
         </article>
       ))}
     </Layout>
@@ -28,7 +28,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
